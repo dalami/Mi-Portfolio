ed941d779ee6f0c9239a271ce7fab4ad18f175e3
Dalami <diegoalami@gmail.com>
2023-07-12 23:15:24 -0300

    imagenes

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Portfolio.css'
+import ivyre from '/src/assets/Images/ivyre.png'
+import Homes from '/src/assets/Images/Homes.png'
+import Gififty from '/src/assets/Images/Gififty.png'
+import galeria from '/src/assets/Images/galeria.png'
+import carrito from '/src/assets/Images/carrito.png'
+import rick from '/src/assets/Images/rick.png'
 
 const Portfolio = () => {
     return (
@@ -7,22 +13,22 @@ const Portfolio = () => {
         <div className='text-center pt-5 mb-5 mt-5' ><h1>Portfolio</h1></div>
         <div className='row justify-content-center'>
             <div className='col-lg-4'>
-               <a href='https://dalami.github.io/trabajp-practico-2/' target='_blank' rel='noreferrer'> <img className='port-img' src="/src/assets/Images/ivyre.png" alt='foto'/></a>
+               <a href='https://dalami.github.io/trabajp-practico-2/' target='_blank' rel='noreferrer'> <img className='port-img' src={ivyre} alt='foto'/></a>
             </div>
             <div className='col-lg-4'>
-                <a href='https://dalami.github.io/Trabajo-practico-3/' target='_blank' rel='noreferrer'> <img className='port-img' src="/src/assets/Images/Homes.png" alt='foto'/></a>
+                <a href='https://dalami.github.io/Trabajo-practico-3/' target='_blank' rel='noreferrer'> <img className='port-img' src={Homes} alt='foto'/></a>
             </div>
             <div className='col-lg-4'>
-            <a href='https://dalami.github.io/trabajo-practico1/' target='_blank' rel='noreferrer'> <img className='port-img' src="/src/assets/Images/Gififty.png" alt='foto'/></a>
+            <a href='https://dalami.github.io/trabajo-practico1/' target='_blank' rel='noreferrer'> <img className='port-img' src={Gififty} alt='foto'/></a>
             </div>
             <div className='col-lg-4'>
-            <a href='https://dalami.github.io/Galeria/' target='_blank' rel='noreferrer'> <img className='port-img' src="/src/assets/Images/galeria.png" alt='foto'/></a>
+            <a href='https://dalami.github.io/Galeria/' target='_blank' rel='noreferrer'> <img className='port-img' src={galeria} alt='foto'/></a>
             </div>
             <div className='col-lg-4'>
-            <a href='https://dalami.github.io/Carrito-de-compra/' target='_blank' rel='noreferrer'> <img className='port-img' src="/src/assets/Images/carrito.png" alt='foto'/></a>
+            <a href='https://dalami.github.io/Carrito-de-compra/' target='_blank' rel='noreferrer'> <img className='port-img' src={carrito} alt='foto'/></a>
             </div>
             <div className='col-lg-4'>
-            <a href='https://dalami.github.io/axios/' target='_blank' rel='noreferrer'> <img className='port-img' src="/src/assets/Images/rick.png" alt='foto'/></a>
+            <a href='https://dalami.github.io/axios/' target='_blank' rel='noreferrer'> <img className='port-img' src={rick} alt='foto'/></a>
             </div>
 </div>
 
